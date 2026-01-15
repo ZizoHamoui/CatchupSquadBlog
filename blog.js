3ccae79342c77a32ff23772f2118ca4667a0ccac
Abdul Aziz Hamoui <149183212+ZizoHamoui@users.noreply.github.com>
2026-01-15 12:44:05 -0800

    Add initial blog post data structure

---
+++ blog.js
@@ -0,0 +1,22 @@
+const blogPosts = [
+  {
+    id: 1,
+    week: "Week 01",
+    date: "Jan 09, 2026",
+    title: "Start of the Journey",
+    content: "We officially formed the team this week albeit a bit later than the rest since most of us were unable to attend the lecture, hence 'catchup squad'. We wasted no time getting started though, and the focus was on coming up with a concrete idea for the game we are developing. More on this coming soon ...",
+    timestamp: "Posted 5:42 PM"
+  },
+  
+  // To add a new card, simply copy the object structure below:
+  /*
+  {
+    id: 2,
+    week: "Week 02",
+    date: "Jan 16, 2026",
+    title: "Prototyping Phase",
+    content: "This week we started working on the Inkwell Insurgency prototype...",
+    timestamp: "Posted 2:15 PM"
+  }
+  */
+];
